73eba29f1cc523ec5f052d5985c99cd755fe78b4
GamingJam <GamingJamTv@gmail.com>
2020-10-13 19:43:20 +0300

    Update BodyNode.cs

--- ILangCompiler/Parser/AST/BodyNode.cs
+++ ILangCompiler/Parser/AST/BodyNode.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Linq.Expressions;
 using ILangCompiler.Parser.AST.Declarations.Types;
+using ILangCompiler.Parser.AST.Expressions;
 using ILangCompiler.Parser.AST.Statements;
 using ILangCompiler.Parser.Exceptions;
 using ILangCompiler.Scanner.Tokens;
+using ILangCompiler.Scanner.Tokens.Predefined.Keywords.Constructions;
 using ILangCompiler.Scanner.Tokens.Predefined.Symbols;
 using LanguageExt;
 
@@ -14,10 +17,16 @@ namespace ILangCompiler.Parser.AST
     public class BodyNode : IAstNode
     {
         public ImmutableArray<IAstNode> Elements;
+        public ExpressionNode ReturnExpressionNode;
 
         private BodyNode(ImmutableArray<IAstNode> elements)
         {
             Elements = elements;
+        }
+        private BodyNode(ImmutableArray<IAstNode> elements, ExpressionNode returnExpressionNode)
+        {
+            Elements = elements;
+            ReturnExpressionNode = returnExpressionNode;
         }
 
         private static ParseException NotABodyException => new ParseException("Not a body");
@@ -74,9 +83,45 @@ namespace ILangCompiler.Parser.AST
                         else break;
                     continue;
                 }
+                
+                var maybeExpression = StatementNode.Parse(tokens);
+                if (maybeExpression.IsRight)
+                {
+                    tokens = maybeExpression.RightToList()[0].First;
+                    elements = elements.Add(maybeExpression.RightToList()[0].Second);
+                    while (tokens.Count > 0)
+                        if (tokens[0] is NewLineSymbolToken || tokens[0] is CommentToken ||
+                            tokens[0] is SemicolonSymbolToken)
+                            tokens = tokens.Skip(1).ToList();
+                        else break;
+                    continue;
+                }
 
                 break;
             }
+            
+            
+            if (tokens.Count < 1)
+                return NotABodyException;
+            if (!(tokens[0] is ReturnKeywordToken))
+                return NotABodyException;
+            tokens = tokens.Skip(1).ToList();
+            var maybeExpression1 = ExpressionNode.Parse(tokens);
+            if (maybeExpression1.IsRight)
+            {
+                
+                while (tokens.Count > 0)
+                    if (tokens[0] is NewLineSymbolToken || tokens[0] is CommentToken ||
+                        tokens[0] is SemicolonSymbolToken)
+                        tokens = tokens.Skip(1).ToList();
+                    else break;
+                tokens = maybeExpression1.RightToList()[0].First;
+                return new Pair<List<IToken>,BodyNode>(tokens, new BodyNode(elements, maybeExpression1.RightToList()[0].Second));
+
+            }  
+            
+            
+            
             return new Pair<List<IToken>,BodyNode>(tokens, new BodyNode(elements));
         }
 
